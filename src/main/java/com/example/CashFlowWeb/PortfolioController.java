3ab2324fd7afd3951e8d05627de0d4b5c5c83f3e
imuimuyuuki <st324a2102y.imura@urayama.ac.jp>
2025-12-05 12:03:04 +0900

    Update PortfolioController.java

--- src/main/java/com/example/CashFlowWeb/PortfolioController.java
+++ src/main/java/com/example/CashFlowWeb/PortfolioController.java
@@ -1,6 +1,7 @@
 package com.example.CashFlowWeb;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -11,36 +12,32 @@ public class PortfolioController {
     private final AssetDAO assetDAO = new AssetDAO();
 
     @GetMapping("/assets")
-    public List<Asset> getAllAssets() {
-        return assetDAO.getAllAssets();
+    public List<Asset> getAllAssets(@AuthenticationPrincipal User user) {
+        return assetDAO.getAllAssets(user.getId());
     }
 
     @GetMapping("/assets/{id}")
-    public ResponseEntity<Asset> getAssetById(@PathVariable int id) {
-        Asset asset = assetDAO.getAssetById(id);
-        if (asset != null) {
-            return ResponseEntity.ok(asset);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+    public ResponseEntity<Asset> getAssetById(@AuthenticationPrincipal User user, @PathVariable int id) {
+        Asset asset = assetDAO.getAssetById(id, user.getId());
+        return asset != null ? ResponseEntity.ok(asset) : ResponseEntity.notFound().build();
     }
 
     @PostMapping("/assets")
-    public ResponseEntity<Boolean> addAsset(@RequestBody Asset asset) {
-        boolean isSuccess = assetDAO.addAsset(asset);
+    public ResponseEntity<Boolean> addAsset(@AuthenticationPrincipal User user, @RequestBody Asset asset) {
+        boolean isSuccess = assetDAO.addAsset(asset, user.getId());
         return isSuccess ? ResponseEntity.ok(true) : ResponseEntity.badRequest().body(false);
     }
 
     @PutMapping("/assets/{id}")
-    public ResponseEntity<Boolean> updateAsset(@PathVariable int id, @RequestBody Asset asset) {
+    public ResponseEntity<Boolean> updateAsset(@AuthenticationPrincipal User user, @PathVariable int id, @RequestBody Asset asset) {
         Asset assetToUpdate = new Asset(id, asset.getName(), asset.getTickerSymbol(), asset.getQuantity(), asset.getPurchasePrice(), asset.getCurrentPrice(), asset.getAssetType());
-        boolean isSuccess = assetDAO.updateAsset(assetToUpdate);
+        boolean isSuccess = assetDAO.updateAsset(assetToUpdate, user.getId());
         return isSuccess ? ResponseEntity.ok(true) : ResponseEntity.badRequest().body(false);
     }
 
     @DeleteMapping("/assets/{id}")
-    public ResponseEntity<Boolean> deleteAsset(@PathVariable int id) {
-        boolean isSuccess = assetDAO.deleteAsset(id);
+    public ResponseEntity<Boolean> deleteAsset(@AuthenticationPrincipal User user, @PathVariable int id) {
+        boolean isSuccess = assetDAO.deleteAsset(id, user.getId());
         return isSuccess ? ResponseEntity.ok(true) : ResponseEntity.badRequest().body(false);
     }
 }
